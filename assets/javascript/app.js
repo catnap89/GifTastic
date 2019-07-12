83f0091695bb18f5c1048bb13db778ca2eaa9508
youngwoo.cho <petshop0907@gmail.com>
2019-07-12 05:10:09 -0400

    Made gifGenerator function that gets data from GIPHY api and retrieve gif data from the object to display on HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,8 +5,8 @@
   * Display text box with submit button -- check
 
 ## When user input text inside text button and click submit button
-  * Dynamically generate button with the text user inputed in the text box.
-  * Append new button to the previous button
+  * Dynamically generate button with the text user inputed in the text box. -- check
+  * Append new button to the previous button -- check
 
 ## When user click on the dynamically generated button
   * Using Giphy API, get the data related to the name of the button clicked
@@ -27,53 +27,78 @@
 // variables
 // ==========================================
 // Store the topic keywords from input box. Gif generator buttons will be made using this array.
-var topics = ["dog", "cat", "duck", "cow"];
+var topics = [];
 
+// ==========================================
+$(".topic-submit").on("click", function(event){
+
+  event.preventDefault();
+  // the value user inputted inside #inputTopic textbox.
+  var topicInput = $("#inputTopic").val().trim();
+  // if the user inputted value is not an empty string and also does not have duplicates in topics array
+  if (topicInput != "" && !topics.includes(topicInput)) {
+    // push the #inputTopic's value as a string in topics array.
+    topics.push(topicInput);
+    // buttonGenerator();
+  }
+  // ??: Does not seem to matter if the function is placed in if statment or outside of it.
+  buttonGenerator();
+
+});
+
+// let's write ajax call to get the data when the dynamically created button is clicked.
+$(document).on("click", ".button-topics", gifGenerator);
+
+buttonGenerator();
 
 // function
 // ==========================================
+
 // function to display buttons
 function buttonGenerator() {
   // Deleting the movie buttons prior to adding new movie buttons
-  // (this is necessary otherwise we will have repeat buttons)
+  // (this is necessary otherwise the previous buttons will remain and newly made buttons will be added on top of them.)
   $(".button-group").empty();
+
   // for loop topics array 
   for(var i = 0; i < topics.length; i++) {
-    // add buttons only if there is some kind of input, and if it is not a duplicate
-    // is it a better idea to use this concept when the input is added to the topics array?
-      var gifButtons = $('<button type="submit" class="btn btn-primary">'+ topics[i] + '</button>');
-  
+      var gifButtons = $('<button type="submit">'+ topics[i] + '</button>');
+      gifButtons.addClass("btn btn-primary button-topics");
+      gifButtons.attr("data-topics", topics[i]);
       $(".button-group").append(gifButtons);
-    // if(topicInput !== empty && topicInput === duplicate) {
-
-
-    // }
-
   }
 
 }
 
-$(".topic-submit").on("click", function(event){
+function gifGenerator() {
+  // topic variable is the value of data-topics of the button that is clicked on.
+  var topic = $(this).attr("data-topics");
+  var limit = 10
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ba6zqaYk7V3NBpJPJXcg5yTDeEf7V0bQ&q=" + topic + "&limit=" + limit;
 
-  event.preventDefault();
-  // the text user inputed inside #inputTopic textbox.
-  var topicInput = $("#inputTopic").val().trim();
-  console.log(topicInput);
-  if (topicInput != "" && !topics.includes(topicInput)) {
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  })
+    .then(function(res){
 
-  // push the #inputTopic's value as a string in topics array.
-  topics.push(topicInput);
+      console.log(res);
+      for(var i = 0; i < limit; i++) {
 
-  buttonGenerator();
-  }
+        var gifURLs = res.data[i].images.fixed_height.url
 
-});
+        var gif = $("<img>");
+
+        //
+        gif.attr("src", gifURLs);
+        // img.attr("alt", "cat image");
+
+        //
+        $(".gif-container").prepend(gif);
 
 
-buttonGenerator();
-// If I keep click on submit button, it makes the button on top of the existing button.
-// Whenever I click on the submit button, I have to push the topic to topics array, however delete current topic buttons and regenerate them.
-
-
+      }
+    });
+}
 
 
